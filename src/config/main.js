57e2e9c7cfeb0a363db37f9b4d2f89229dfa333c
Ellery Ricaurte <elleryrica12@gmail.com>
2023-11-09 21:28:15 -0500

    Markdown

---
+++ src/config/main.js
@@ -0,0 +1,6 @@
+// main.js
+const showdown = require('showdown');
+const converter = new showdown.Converter();
+const text = '# Hello, Markdown!';
+const html = converter.makeHtml(text);
+console.log(html);
