cb7258e192d43974a1a3bbde7990de8f2feaa9ef
sambayoyo <csamuert@gmail.com>
2023-11-12 14:29:23 -0500

    mejorando el front

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder="config/templates", static_folder="config/
 
 @app.route("/", methods=['GET'])
 def index():
-    return render_template("editor_syllabus.html")
+    return render_template("editor_botones.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
